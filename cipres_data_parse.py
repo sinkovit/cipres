f4685aa091f6e922e2da994454be0aeeeeefc827
sinkovit <sinkovit@sdsc.edu>
2015-11-04 14:34:23 -0800

    code cleanup and addition of universal newline support

--- cipres_data_parse.py
+++ cipres_data_parse.py
@@ -4,15 +4,19 @@
 # to CIPRES gateway. Currently configued only to handle BEAUTi files,
 # but will be extended to other file formats
 #
+# Note that all files are opened with universal newlines support
+# ('rU') so that we can handle files created using the Linux, Windows
+# and Mac formats
+#
 # Author: Robert Sinkovits, SDSC
 #
 # Usage: cipres_data_parse [filename] 
 
 import re
 import sys
-filename = sys.argv[1]
+file_name = sys.argv[1]
 
-def process_beauti(filename):
+def process_beauti(file_name):
 
     # Process BEAUTi format files and return following
     #
@@ -33,7 +37,7 @@ def process_beauti(filename):
     regex_patterns = '.*npatterns\s*=\s*'
     regex_codon    = 'codon'
 
-    with open(filename, 'r') as fin:
+    with open(file_name, 'rU') as fin:
         for line in fin:
             line = line.rstrip()
 
@@ -54,25 +58,24 @@ def process_beauti(filename):
             if re.search(regex_codon, line):
                 is_codon = True
 
-    print 'data type:            ', data_type
-    print 'Is codon file:        ', is_codon
-    print 'number of partitions: ', partition_count
-    print 'number of patterns:   ', pattern_count
     return data_type, is_codon, partition_count, pattern_count
 
 
 # Determine the input file type
-filetype = 'unknown'
-with open(filename, 'r') as fin:
+file_type = 'unknown'
+with open(file_name, 'rU') as fin:
     for line in fin:
         if line.find('BEAUTi') >= 0:
-            filetype = 'beauti'
+            file_type = 'beauti'
             break
 
-print 'file type: ', filetype
-
-# Loop over records in BEAUTi file
-if filetype == 'beauti':
-    process_beauti(filename)
+# Process BEAUTi files
+if file_type == 'beauti':
+    data_type, is_codon, partition_count, pattern_count = process_beauti(file_name)
+    print 'file type:            ', file_type
+    print 'data type:            ', data_type
+    print 'Is codon file:        ', is_codon
+    print 'number of partitions: ', partition_count
+    print 'number of patterns:   ', pattern_count
 
 
